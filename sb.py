3d35f2e4fe9f047a3f76a6f72c3a354f50ed0a84
NoahRGB <ncox2004@gmail.com>
2026-06-25 10:04:52 +0100

    minor changes

--- sb.py
+++ sb.py
@@ -8,7 +8,7 @@ from sb3_contrib import RecurrentPPO
 from sb3_contrib import RecurrentPPO
 
 import numpy as np
-import pickle
+import pickle, pathlib
 import gymnasium as gym
 
 from envs import POMDPCartPole
@@ -41,7 +41,6 @@ class EpisodeRewardLogger(BaseCallback):
         return True
 
     def save(self, path):
-        import pathlib, pickle
         pathlib.Path(path).mkdir(parents=True, exist_ok=True)
         for var_name, value in self.vars.items():
             with open(f"{path}/{var_name}.pkl", "wb") as f:
@@ -56,29 +55,37 @@ def linear_schedule(initial_value):
 LOG_PATH = "./runs"
 SEED = 1
 TIMESTEPS = 100000
-NUM_ENVS = 8
+NUM_ENVS = 1
 STATS_WINDOW = 100
 
 # env = gym.make("Pendulum-v1", render_mode=None)
-vec_env = make_vec_env("POMDPCartPole", n_envs=NUM_ENVS, seed=SEED)
-vec_env = VecNormalize(vec_env)
+vec_env = make_vec_env("CartPole-v1", n_envs=NUM_ENVS, seed=SEED)
+# vec_env = VecNormalize(vec_env)
 
-agent = RecurrentPPO("MlpLstmPolicy", vec_env, learning_rate=0.001, gamma=0.98, gae_lambda=0.8, n_steps=32,
-                   ent_coef=0.0, vf_coef=0.5, max_grad_norm=0.5,
-                   n_epochs=20, batch_size=256,
-                   tensorboard_log=LOG_PATH,
-                   seed=SEED,
-                   clip_range=linear_schedule(0.2),
-                   policy_kwargs=dict(
-                        ortho_init=False,
-                        activation_fn=torch.nn.ReLU,
-                        lstm_hidden_size=64,
-                        enable_critic_lstm=True,
-                        net_arch=dict(pi=[64], vf=[64])
-                    ),)
+# agent = RecurrentPPO("MlpLstmPolicy", vec_env, learning_rate=0.001, gamma=0.98, gae_lambda=0.8, n_steps=32,
+#                    ent_coef=0.0, vf_coef=0.5, max_grad_norm=0.5,
+#                    n_epochs=20, batch_size=256,
+#                    tensorboard_log=LOG_PATH,
+#                    seed=SEED,
+#                    clip_range=linear_schedule(0.2),
+#                    policy_kwargs=dict(
+#                         ortho_init=False,
+#                         activation_fn=torch.nn.ReLU,
+#                         lstm_hidden_size=64,
+#                         enable_critic_lstm=True,
+#                         net_arch=dict(pi=[64], vf=[64])
+#                     ),)
 
 # agent = TD3("MlpPolicy", vec_env, gamma=0.98, buffer_size=200000, learning_starts=10000, target_policy_noise=0.1, stats_window_size=STATS_WINDOW, seed=SEED, tensorboard_log=LOG_PATH)
 # agent = A2C("MlpPolicy", vec_env, ent_coef=0.0, stats_window_size=STATS_WINDOW, seed=SEED, tensorboard_log=LOG_PATH)
+
+agent = DQN("MlpPolicy", vec_env, learning_rate=0.0023, batch_size=64,
+            gamma=0.99, buffer_size=100000, learning_starts=1000, train_freq=256, gradient_steps=128,
+            target_update_interval=10, exploration_fraction=0.16, exploration_final_eps=0.04,
+            stats_window_size=STATS_WINDOW, seed=SEED, tensorboard_log=LOG_PATH,
+            policy_kwargs=dict(
+                net_arch=[256, 256]
+            ),)
 
 print(agent.policy)
 
@@ -86,4 +93,4 @@ logger = EpisodeRewardLogger()
 
 trained_agent = agent.learn(total_timesteps=TIMESTEPS, callback=logger)
 
-# logger.save(f"./results/temps/data/sb3_a2c_cartpole_seed{SEED}_timesteps{TIMESTEPS}")
+logger.save(f"./results/temps/data/sb3_dqn_cartpole_seed{SEED}")
